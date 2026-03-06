6ab64717d4c4905fc41c11b5b7da1e56d5102c1c
ratthaphum778 <sb6740259112@lru.ac.th>
2026-03-06 23:31:49 +0700

    model.py

--- model.py
+++ model.py
@@ -666,7 +666,9 @@ def create_default_admin():
         c.execute(
             """
             INSERT INTO users (username, password_hash, role, is_active)
-            VALUES ('teacher', '1234', 'admin', 1)
+            VALUES ('teacher',
+            '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4',
+            'admin', 1)
             """
         )
 
